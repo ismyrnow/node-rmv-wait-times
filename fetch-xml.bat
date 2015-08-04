c74648635d555f4bd18626e324c9e585e60fb2c8
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-08-04 15:08:45 -0400

    Delete windows batch script
    - Moved to gist: https://gist.github.com/ismyrnow/b23a8a114672d704ebf3

--- fetch-xml.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set timestamp=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%-%ldt:~8,2%-%ldt:~10,2%-%ldt:~12,2%
-echo %timestamp%
-curl http://www.massdot.state.ma.us/feeds/qmaticxml/qmaticXML.aspx > .\%timestamp%.xml
